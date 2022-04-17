dff2a5c68814682cba2f777bb1ac2d599817fbce
MikhailYuzhakov <miha.yuzhakov@gmail.com>
2022-04-17 20:56:58 +0300

    Тест на четность

---
+++ Task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным.
+*/
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0) {
+    Console.Write("Число "); Console.Write(a);
+    Console.WriteLine(" является четныем");
+} else {
+    Console.Write("Число "); Console.Write(a);
+    Console.WriteLine(" является нечетныем");
+}
