6affca1721875db9e4749f10dfbb352333f8f809
MikhailYuzhakov <miha.yuzhakov@gmail.com>
2022-04-17 20:52:53 +0300

    Максимальное из трёх

---
+++ Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+*/
+
+Console.Clear();
+Console.WriteLine("Введите 3 числа для сравнения: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("Максимальное число = ");
+Console.WriteLine(max);
